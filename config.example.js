a45ad796423fe6fa6491fd6cbca59ccef5455ee2
studentacs05-cpu <student.acs05@gmail.com>
2026-09-19 23:58:03 -0600

    Add files via upload

---
+++ config.example.js
@@ -0,0 +1,5 @@
+window.TRACKER_CONFIG = {
+  supabaseUrl: "https://YOUR_PROJECT.supabase.co",
+  supabaseAnonKey: "YOUR_PUBLIC_ANON_KEY",
+  aiEnabled: false
+};
---
+++ config.js
@@ -0,0 +1,5 @@
+window.TRACKER_CONFIG = {
+  supabaseUrl: "csfrpadcaanayhjtudxy",
+  supabaseAnonKey: "sb_publishable_MObo0TNcYbYElSSXAXX65w_h8QojajY",
+  aiEnabled: false
+};
